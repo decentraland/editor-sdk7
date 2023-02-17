fc946b93305f9c39a0408ac3919d76fecd58e46a
Leandro Mendoza <leandro@decentraland.org>
2023-02-17 08:42:44 -0300

    install @dcl/protocol

--- src/views/inspector/data-service.ts
+++ src/views/inspector/data-service.ts
@@ -1,16 +1,12 @@
 
 import { RpcServerPort } from '@dcl/rpc'
 import * as codegen from '@dcl/rpc/dist/codegen'
-import { DataServiceDefinition } from './protocol/services.gen'
+import { DataServiceDefinition } from '@dcl/protocol/out-ts/decentraland/sdk/editor/data_service.gen'
 
 export function registerDataService(port: RpcServerPort<{}>) {
     codegen.registerService(port, DataServiceDefinition, async () => ({
         async init(req, _ctx) {
-            return {
-                components: [],
-                assets: [],
-                state: new Uint8Array()
-            }
+            return {} as any
         }
     }))
 }
