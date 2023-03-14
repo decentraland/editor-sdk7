d308d42939cd9816d0693679d6f831d214b159a6
Juan Cazala <juancazala@gmail.com>
2023-03-14 13:48:07 -0300

    feat: added util to notify updates of a module

---
+++ src/modules/notification.ts
@@ -0,0 +1,40 @@
+import * as vscode from 'vscode'
+import semver from 'semver'
+import { getPackageJson } from './pkg'
+import { getGlobalValue, setGlobalValue } from './storage'
+
+export async function notifyUpdate(
+  title: string,
+  moduleName: string,
+  storage: string,
+  repo: string
+) {
+  // Grab version from last activation
+  const prevVersion = getGlobalValue<string>(storage)
+
+  // Grab current versions
+  const currentVersion = getPackageJson(moduleName).version
+
+  // Update stored versions for next activation
+  setGlobalValue(storage, currentVersion)
+
+  if (!prevVersion) {
+    // No need to notify anything the first activation
+    return
+  }
+
+  // Check if the has been an upgrade in the version and notify
+  if (semver.gt(currentVersion, prevVersion)) {
+    const didClick = await vscode.window.showInformationMessage(
+      `${title} has been updated to v${currentVersion}!`,
+      `Learn More`
+    )
+    if (didClick) {
+      await vscode.env.openExternal(
+        vscode.Uri.parse(
+          `https://github.com/${repo}/releases/tag/${currentVersion}`
+        )
+      )
+    }
+  }
+}
