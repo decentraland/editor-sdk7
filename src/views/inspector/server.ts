8c6ca49ee002a2c3d3d51d63a3e048aa43bd45f5
Juan Cazala <juancazala@gmail.com>
2023-05-16 10:26:58 -0300

    fix: support inspector installed in a parent directory

--- src/views/inspector/server.ts
+++ src/views/inspector/server.ts
@@ -2,7 +2,12 @@ import path from 'path'
 import { StaticServer } from '../../modules/server'
 import { ServerName } from '../../types'
 import { getCwd } from '../../modules/workspace'
+import { log } from '../../modules/log'
 
-export const inspectorServer = new StaticServer(ServerName.Inspector, () =>
-  path.join(getCwd(), './node_modules/@dcl/inspector/public')
-)
+export const inspectorServer = new StaticServer(ServerName.Inspector, () => {
+  const inspectorPath = path.dirname(
+    require.resolve('@dcl/inspector/package.json', { paths: [getCwd()] })
+  )
+  log('Serving inspector from:', inspectorPath)
+  return path.join(inspectorPath, './public')
+})
