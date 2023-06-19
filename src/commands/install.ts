cf4cd29852e87b5504a5642e30cbf5fd5ee5c91f
Nicolas Echezarreta <nicoecheza@gmail.com>
2023-06-19 10:53:40 -0300

    fix: install package placeholder

--- src/commands/install.ts
+++ src/commands/install.ts
@@ -4,7 +4,7 @@ import { npmInstall } from '../modules/npm'
 export async function install() {
   const dependency = await vscode.window.showInputBox({
     title: 'Install package',
-    placeHolder: '@dcl/ecs-scene-utils',
+    placeHolder: '@dcl-sdk/utils',
     prompt: 'Enter the name of the package',
   })
   if (!dependency) {
--- src/commands/uninstall.ts
+++ src/commands/uninstall.ts
@@ -4,7 +4,7 @@ import { npmUninstall } from '../modules/npm'
 export async function uninstall() {
   const dependency = await vscode.window.showInputBox({
     title: 'Uninstall package',
-    placeHolder: '@dcl/ecs-scene-utils',
+    placeHolder: '@dcl-sdk/utils',
     prompt: 'Enter the name of the package',
   })
   if (dependency) {
