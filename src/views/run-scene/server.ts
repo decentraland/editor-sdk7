77d904a9c88ed964d001586f7a7e0728b3377477
Gonzalo DCL <gonzalo@decentraland.org>
2025-03-13 15:08:52 -0300

    show error message

--- src/views/run-scene/server.ts
+++ src/views/run-scene/server.ts
@@ -8,6 +8,7 @@ import { hasNodeModules } from '../../modules/workspace'
 import { syncSdkVersion } from '../../modules/sdk'
 import { Server } from '../../modules/server'
 import { ServerName } from '../../types'
+import { useCreatorHubMessage } from '../../utils'
 
 class RunSceneServer extends Server {
   child: SpanwedChild | null = null
@@ -17,7 +18,9 @@ class RunSceneServer extends Server {
 
   async onStart() {
     const a = true
-    if (a) return
+    if (a) {
+      return useCreatorHubMessage()
+    }
     // install dependencies
     if (!hasNodeModules()) {
       await npmInstall()
